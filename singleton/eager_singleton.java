0127ed059e4666551b1ef3edb8730d7236f84141
builephilong69 <80462415+builephilong69@users.noreply.github.com>
2022-03-19 05:41:09 +0700

    Delete eager_singleton.java

--- singleton/eager_singleton.java
+++
@@ -1,8 +0,0 @@
-class EagerSingleton{
-  private EagerSingleton ES_instance = new EagerSingleton();
-  private static EagerSingleton(){}
-  
-  public static getInstance(){
-    return this.ES_instance;
-   }
-}
